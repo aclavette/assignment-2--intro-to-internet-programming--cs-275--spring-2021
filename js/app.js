94c724b54c0999a494eb869d9c306653df286c66
aclavette <aclavette@users.noreply.github.com>
2021-04-02 17:50:01 -0400

    Moved js to correct file location

--- js/app.js
+++ js/app.js
@@ -1,55 +1,3 @@
-const { src } = require(`gulp`);
-const htmlValidator = require(`gulp-html`);
-const cssLinter = require(`gulp-stylelint`);
-const jsLinter = require(`gulp-eslint`);
-const babel = require(`gulp-babel`);
-const htmlCompressor = require(`gulp-htmlmin`);
-
-let validateHTML = () => {
-    return src(`../html/index.html`)
-        .pipe(htmlValidator());
-};
-
-let compressHTML = () => {
-    return src(`../html/index.html`)
-        .pipe(htmlCompressor({collapseWhitespace: true}))
-        .pipe(dest(`../prod/html/`));
-};
-
-let lintCSS = () => {
-    return src(`css/style.css`)
-        .pipe(cssLinter({
-            failAfterError: true,
-            reporters: [
-                {formatter: `verbose`, console: true}
-            ]
-        }));
-};
-
-let lintJS = () => {
-    return src(`app.js`)
-        .pipe(jsLinter({
-            parserOptions: {
-                ecmaVersion: 2017,
-                sourceType: `module`
-            },
-            rules: {
-                indent: [2, 4, {SwitchCase: 1}],
-                quotes: [2, `backtick`],
-                semi: [2, `always`],
-                'linebreak-style': [2, `unix`],
-                'max-len': [1, 85, 4]
-            },
-            env: {
-                es6: true,
-                node: true,
-                browser: true
-            },
-            extends: `eslint:recommended`
-        }))
-        .pipe(jsLinter.formatEach(`compact`, process.stderr));
-};
-
 function whiterice() {
     document.getElementById("wr").style.visiblity = "hidden";
 }
@@ -57,8 +5,3 @@ function whiterice() {
 function sproutedrice() {
     document.getElementById("scr").style.visibility = "visible";
 }
-
-exports.validateHTML = validateHTML;
-exports.compressHTML = compressHTML;
-exports.cssLinter = cssLinter;
-exports.lintJS = lintJS;
