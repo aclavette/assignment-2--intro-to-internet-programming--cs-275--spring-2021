7eb27e119402907f28f11ec96ce055009267cc4c
aclavette <aclavette@users.noreply.github.com>
2021-04-02 17:26:05 -0400

    Added first attempt at recipe functions

--- js/app.js
+++ js/app.js
@@ -24,4 +24,12 @@ let jsValidator = () => {
 
 };
 
+function whiterice() {
+    document.getElementById("wr").style.visiblity = "hidden";
+}
+
+function sproutedrice() {
+    document.getElementById("scr").style.visibility = "visible";
+}
+
 exports.validateHTML = validateHTML;
