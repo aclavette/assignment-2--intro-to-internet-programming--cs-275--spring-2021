839c55ced109f3a8fdc63be59ae8c7ae8a47ef70
aclavette <aclavette@users.noreply.github.com>
2021-04-07 14:30:39 -0400

    Made recipes appear on click

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 function whiterice() {
-    document.getElementById("wr").style.visiblity = "hidden";
+	let recipe = document.getElementById("content");
+	recipe.textContent =
+	"White Rice: Combine 1 cup of rice with 2 cups of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about 18 minutes.";
 }
 
 function sproutedrice() {
-    document.getElementById("scr").style.visibility = "visible";
+    let recipe = document.getElementById("content");
+    recipe.innerHTML =
+    "Sprouted California Rice: For slightly al dente rice: Combine 1 1/4 cups of rice with 2 cups of water or broth and 1 Tbsp olive oil. Bring to a boil and stir once to mix. Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes. Remove from heat and let stand for 5 minutes. Fluff with a fork and serve. <br><br>For softer rice: Increase liquid by 1/2 cup and cook time by 5 minutes.";
 }
