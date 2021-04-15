d81aca17dff0b19942e71984de69d8e67e3965cb
aclavette <aclavette@users.noreply.github.com>
2021-04-15 11:13:34 -0400

    Added event listeners for buttons

--- js/app.js
+++ js/app.js
@@ -1,21 +1,38 @@
+window.onload = () => {
+    let button1 = document.querySelector("#whiterice");
+    let button2 = document.querySelector("#sproutedrice");
+    button1.addEventListener(`click`, whiterice);
+    button2.addEventListener(`click`, sproutedrice);
+}
+
 function whiterice() {
-	let recipe = document.getElementById("content");
-	recipe.textContent =
-	"White Rice: Combine 1 cup of rice with 2 cups of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about 18 minutes.";
-  let input = document.getElementById("form");
-	input.style.visibility="visible";
-	let rice = document.getElementById("form").value;
+	let content = document.getElementById("content");
+	content.innerHTML =
+	"Combine 1 cup of rice with 2 cups of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about 18 minutes.<br><br>You will need 2 times more water than rice.";
+
+    let form = document.getElementById("form");
+	form.style.visibility="visible";
+
+	let rice = parseInt(document.getElementById("form").value, 10);
 	let water = rice*2;
 	document.getElementById("result").innerHTML = "You will need " + water + " cups of water.";
 }
 
 function sproutedrice() {
-    let recipe = document.getElementById("content");
-    recipe.innerHTML =
-    "Sprouted California Rice: For slightly al dente rice: Combine 1 1/4 cups of rice with 2 cups of water or broth and 1 Tbsp olive oil. Bring to a boil and stir once to mix. Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes. Remove from heat and let stand for 5 minutes. Fluff with a fork and serve. <br><br>For softer rice: Increase liquid by 1/2 cup and cook time by 5 minutes.";
-    let input = document.getElementById("form");
-	  input.style.visibility="visible";
-	  let rice = document.getElementById("form").value;
-	  let water = rice*1.6;
-	  document.getElementById("result").innerHTML = "You will need " + water + " cups of water.";
+    let content = document.getElementById("content");
+    content.innerHTML =
+    "For slightly al dente rice: Combine 1 1/4 cups of rice with 2 cups of water or broth and 1 Tbsp olive oil. Bring to a boil and stir once to mix. Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes. Remove from heat and let stand for 5 minutes. Fluff with a fork and serve. <br><br>For softer rice: Increase liquid by 1/2 cup and cook time by 5 minutes.<br><br>You will need 1.6 times more water than rice.";
+
+    let form = document.getElementById("form");
+	form.style.visibility="visible";
+
+    let rice = parseInt(document.getElementById("form").value, 10);
+	let water = rice*1.6;
+	document.getElementById("result").innerHTML = "You will need " + water + " cups of water.";
+}
+
+function calculate(){
+	let rice = parseInt(document.getElementById("form").value, 10);
+	let answer = rice*1.6;
+	document.getElementById("result").innerHTML = answer;
 }
