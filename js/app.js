1ae3f665102297af465095defc499d5e508e8d99
aclavette <aclavette@users.noreply.github.com>
2021-04-16 13:59:28 -0400

    Finished all JS

--- js/app.js
+++ js/app.js
@@ -2,13 +2,15 @@ window.onload = () => {
     let button1 = document.querySelector("#whiterice");
     let button2 = document.querySelector("#sproutedrice");
     let form = document.querySelector("#form");
+    let input = document.querySelector(`input`);
+    let output = document.querySelector("#result");
     button1.addEventListener(`click`, whiteRice);
     button2.addEventListener(`click`, sproutedRice);
 }
 
 function whiteRice() {
     function calculateWR() {
-        let rice = parseInt(document.getElementById("form").value, 10);
+        let rice = input.value;
     	let answer = rice*2;
         document.getElementById("result").innerHTML = "You will need " + answer + " cups of water.";
     }
@@ -17,15 +19,13 @@ function whiteRice() {
 	content.innerHTML =
 	"Combine 1 cup of rice with 2 cups of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about 18 minutes.<br><br>You will need 2 times more water than rice.";
 
-    let form = document.getElementById("form");
 	form.style.visibility="visible";
-
     form.addEventListener(`keyup`, calculateWR);
 }
 
 function sproutedRice() {
     function calculateSR() {
-        let rice = parseInt(document.getElementById("form").value, 10);
+        let rice = input.value;
     	let answer = rice*1.6;
         document.getElementById("result").innerHTML = "You will need " + answer + " cups of water.";
     }
@@ -34,8 +34,6 @@ function sproutedRice() {
     content.innerHTML =
     "For slightly al dente rice: Combine 1 1/4 cups of rice with 2 cups of water or broth and 1 Tbsp olive oil. Bring to a boil and stir once to mix. Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes. Remove from heat and let stand for 5 minutes. Fluff with a fork and serve. <br><br>For softer rice: Increase liquid by 1/2 cup and cook time by 5 minutes.<br><br>You will need 1.6 times more water than rice.";
 
-    let form = document.getElementById("form");
 	form.style.visibility="visible";
-
     form.addEventListener(`keyup`, calculateSR);
 }
